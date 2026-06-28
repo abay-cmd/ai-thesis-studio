bd5d6160963edb54172df288d9e44968e7ccffc1
abay-cmd <akbarrido.ar@gmail.com>
2026-06-29 00:31:05 +0700

    Update server.ts

--- server.ts
+++ server.ts
@@ -26,7 +26,7 @@ const upload = multer({ storage: storage });
 
 async function startServer() {
   const app = express();
-  const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
 
   app.use(express.json({ limit: "50mb" }));
 
